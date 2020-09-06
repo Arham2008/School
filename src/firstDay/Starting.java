fbdf49b44c6023c1f179f1ea0b47c82fd09c404f
arham2008 <akbarmunner@gmail.com>
2020-09-06 15:06:30 -0400

    i add school project

---
+++ src/firstDay/Starting.java
@@ -0,0 +1,27 @@
+package firstDay;
+
+public class Starting {
+
+	public static void main(String[] args) {
+		int td=109;
+		long fc=34566645l;
+		float price=5.99f;
+		double cd= 243.99;
+		byte  md= 127;
+		short flo=211;
+		char pri='L';
+		boolean check=true;
+		
+		System.out.println(td);
+		System.out.println(fc);
+		System.out.println(price);
+		System.out.println(cd);
+		System.out.println(md);
+		System.out.println(flo);
+		System.out.println(pri);
+		System.out.println(check);
+		
+
+	}
+
+}
